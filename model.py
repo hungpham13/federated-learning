c24d973794a71d5febd482bdafc523a72b1b48d0
hung <tonic1307@gmail.com>
2023-06-09 15:59:39 +0700

    hehe

--- model.py
+++ model.py
@@ -73,3 +73,71 @@ class Net(nn.Module):
         loss /= len(testloader.dataset)
         accuracy = correct / total
         return loss, accuracy
+
+
+class VGG16(nn.Module):
+    cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.features = self._make_layers(self.cfg)
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+    def train_epoch(self, trainloader, epochs: int):
+        """Train the network on the training set."""
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.parameters())
+        self.train()
+        for epoch in range(epochs):
+            correct, total, epoch_loss = 0, 0, 0.0
+            for images, labels in trainloader:
+                images, labels = images.to(DEVICE), labels.to(DEVICE)
+                optimizer.zero_grad()
+                outputs = self(images)
+                loss = criterion(self(images), labels)
+                loss.backward()
+                optimizer.step()
+                # Metrics
+                epoch_loss += loss
+                total += labels.size(0)
+                correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+            epoch_loss /= len(trainloader.dataset)
+            epoch_acc = correct / total
+            print(f"Epoch {epoch + 1}: train loss {epoch_loss}, accuracy {epoch_acc}")
+
+    def test(self, testloader):
+        """Evaluate the network on the entire test set."""
+        criterion = torch.nn.CrossEntropyLoss()
+        correct, total, loss = 0, 0, 0.0
+        self.eval()
+        with torch.no_grad():
+            for images, labels in testloader:
+                images, labels = images.to(DEVICE), labels.to(DEVICE)
+                outputs = self(images)
+                loss += criterion(outputs, labels).item()
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        loss /= len(testloader.dataset)
+        accuracy = correct / total
+        return loss, accuracy
